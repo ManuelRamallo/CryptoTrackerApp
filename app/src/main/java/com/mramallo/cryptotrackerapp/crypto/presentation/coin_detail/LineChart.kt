34c4809e72f3da33684ba758fab5c76b0d55e918
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-04-03 21:11:51 +0200

    WIP: Init draw line chart with labels

--- app/src/main/java/com/mramallo/cryptotrackerapp/crypto/presentation/coin_detail/LineChart.kt
+++ app/src/main/java/com/mramallo/cryptotrackerapp/crypto/presentation/coin_detail/LineChart.kt
@@ -1,7 +1,34 @@
 package com.mramallo.cryptotrackerapp.crypto.presentation.coin_detail
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mramallo.cryptotrackerapp.crypto.domain.CoinPrice
+import com.mramallo.cryptotrackerapp.ui.theme.CryptoTrackerAppTheme
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+import kotlin.random.Random
 
 @Composable
 fun LineChart(
@@ -15,5 +42,142 @@ fun LineChart(
     onXLabelWidthChange: (Float) -> Unit = {},
     showHelperLines: Boolean = true
 ) {
+    val textStyle = LocalTextStyle.current.copy(
+        fontSize = style.labelFontSize
+    )
 
+    val visibleDataPoints = remember(dataPoints, visibleDataPointsIndices) {
+        dataPoints.slice(visibleDataPointsIndices)
+    }
+
+    val maxYValue = remember(visibleDataPoints) {
+        visibleDataPoints.maxOfOrNull { it.y } ?: 0f
+    }
+
+    val minYValue = remember(visibleDataPoints) {
+        visibleDataPoints.minOfOrNull { it.y } ?: 0f
+    }
+
+    val measurer = rememberTextMeasurer()
+
+    var xLabelWidth by remember {
+        mutableFloatStateOf(0f)
+    }
+    LaunchedEffect(key1 = xLabelWidth) {
+        onXLabelWidthChange(xLabelWidth)
+    }
+
+    val selectedDataPointIndex = remember(selectedDataPoint) {
+        dataPoints.indexOf(selectedDataPoint)
+    }
+    var drawPoints by remember {
+        mutableStateOf(listOf<DataPoint>())
+    }
+    var isShowingDataPoints by remember {
+        mutableStateOf(selectedDataPoint != null)
+    }
+
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        val minLabelSpacingYPx = style.minYLabelSpacingDp.roundToPx()
+        val verticalPaddingPx = style.verticalPadding.roundToPx()
+        val horizontalPaddingPx = style.horizontalPadding.roundToPx()
+        val xAxisLabelSpacingPx = style.xAxisLabelSpacing.toPx()
+
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
+            )
+        }
+        val maxXLabelWidth = xLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        val maxXLabelHeight = xLabelTextLayoutResults.maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCounts = xLabelTextLayoutResults.maxOfOrNull { it.lineCount } ?: 0
+        val xLabelLineHeight = maxXLabelHeight / maxXLabelLineCounts
+
+        val viewPortHeightPx = size.height -
+                (maxXLabelHeight + 2 * verticalPaddingPx
+                        + xLabelLineHeight + xAxisLabelSpacingPx)
+        val viewPortTopY = verticalPaddingPx + xLabelLineHeight + 10f
+        val viewPortRightX = size.width
+        val viewPortBottomY = viewPortTopY + viewPortHeightPx
+        val viewPortLeftX = 2f * horizontalPaddingPx
+        val viewPort = Rect(
+            left = viewPortLeftX,
+            top = viewPortTopY,
+            right = viewPortRightX,
+            bottom = viewPortBottomY
+        )
+
+        drawRect(
+            color = Color.Green,
+            topLeft = viewPort.topLeft,
+            size = viewPort.size
+        )
+
+        xLabelWidth = maxXLabelWidth + xAxisLabelSpacingPx
+        xLabelTextLayoutResults.forEachIndexed { index, result ->
+            drawText(
+                textLayoutResult = result,
+                topLeft = Offset(
+                    x = viewPortLeftX + xAxisLabelSpacingPx / 2f + xLabelWidth * index,
+                    y = viewPortBottomY + xAxisLabelSpacingPx
+                )
+            )
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+private fun LineChartPreview() {
+    CryptoTrackerAppTheme {
+        val coinHistoryRandomized = remember {
+            (1..20).map {
+                CoinPrice(
+                    priceUsd = Random.nextFloat() * 1000.0,
+                    dateTime = ZonedDateTime.now().plusHours(it.toLong())
+                )
+            }
+        }
+        val style = ChartStyle(
+            charLineColor = Color.Blue,
+            unselectedColor = Color(0xFF7C7C7C),
+            selectedColor = Color.Black,
+            helperLinesThicknessPx = 5f,
+            axisLinesThicknessPx = 5f,
+            labelFontSize = 14.sp,
+            minYLabelSpacingDp = 25.dp,
+            verticalPadding = 8.dp,
+            horizontalPadding = 8.dp,
+            xAxisLabelSpacing = 8.dp
+        )
+        val dataPoints = remember {
+            coinHistoryRandomized.map {
+                DataPoint(
+                    x = it.dateTime.hour.toFloat(),
+                    y = it.priceUsd.toFloat(),
+                    xLabel = DateTimeFormatter
+                        .ofPattern("ha\nM/d")
+                        .withLocale(Locale.US)
+                        .format(it.dateTime)
+                )
+            }
+        }
+        LineChart(
+            dataPoints = dataPoints,
+            style = style,
+            visibleDataPointsIndices = 0..19,
+            unit = "$",
+            modifier = Modifier
+                .width(700.dp)
+                .height(300.dp)
+                .background(Color.White),
+            selectedDataPoint = dataPoints[1]
+        )
+    }
 }
