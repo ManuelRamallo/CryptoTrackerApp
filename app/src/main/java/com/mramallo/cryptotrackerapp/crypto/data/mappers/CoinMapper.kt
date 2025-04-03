1c1b1b2d981a76bb3944076ff9ab9ff515a352f0
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-04-03 19:54:49 +0200

    Feature: Loading coin details

--- app/src/main/java/com/mramallo/cryptotrackerapp/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/mramallo/cryptotrackerapp/crypto/data/mappers/CoinMapper.kt
@@ -1,7 +1,11 @@
 package com.mramallo.cryptotrackerapp.crypto.data.mappers
 
 import com.mramallo.cryptotrackerapp.crypto.data.networking.dto.CoinDto
+import com.mramallo.cryptotrackerapp.crypto.data.networking.dto.CoinPriceDto
 import com.mramallo.cryptotrackerapp.crypto.domain.Coin
+import com.mramallo.cryptotrackerapp.crypto.domain.CoinPrice
+import java.time.Instant
+import java.time.ZoneId
 
 fun CoinDto.toCoin(): Coin {
     return Coin(
@@ -13,5 +17,14 @@ fun CoinDto.toCoin(): Coin {
         priceUsd = priceUsd ?: 0.0,
         changePercent24Hr = changePercent24Hr ?: 0.0,
         explorer = explorer ?: ""
+    )
+}
+
+fun CoinPriceDto.toCoinPrice(): CoinPrice {
+    return CoinPrice(
+        priceUsd = priceUsd,
+        dateTime = Instant
+            .ofEpochMilli(time)
+            .atZone(ZoneId.of("UTC"))
     )
 }
--- app/src/main/java/com/mramallo/cryptotrackerapp/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/mramallo/cryptotrackerapp/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.mramallo.cryptotrackerapp.core.domain.util.NetworkError
 import com.mramallo.cryptotrackerapp.core.domain.util.Result
 import com.mramallo.cryptotrackerapp.core.domain.util.map
 import com.mramallo.cryptotrackerapp.crypto.data.mappers.toCoin
+import com.mramallo.cryptotrackerapp.crypto.data.mappers.toCoinPrice
+import com.mramallo.cryptotrackerapp.crypto.data.networking.dto.CoinsHistoryDto
 import com.mramallo.cryptotrackerapp.crypto.data.networking.dto.CoinsResponseDto
 import com.mramallo.cryptotrackerapp.crypto.domain.Coin
 import com.mramallo.cryptotrackerapp.crypto.domain.CoinDataSource
+import com.mramallo.cryptotrackerapp.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
@@ -23,6 +29,27 @@ class RemoteCoinDataSource(
             )
         }.map { response ->
             response.data.map { it.toCoin() }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start.withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli()
+        val endMillis = end.withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli()
+
+        return safeCall<CoinsHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6")
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
         }
     }
 
