71e0402c10ebca2a45113f15bf7cc689fd0811f2
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-04-08 22:52:34 +0200

    WIP: Continue on empty state design

--- app/src/main/java/com/mramallo/cryptotrackerapp/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/mramallo/cryptotrackerapp/crypto/presentation/coin_list/CoinListScreen.kt
@@ -1,10 +1,17 @@
 package com.mramallo.cryptotrackerapp.crypto.presentation.coin_list
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -20,8 +27,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mramallo.cryptotrackerapp.R
 import com.mramallo.cryptotrackerapp.crypto.presentation.coin_list.components.CoinListItem
 import com.mramallo.cryptotrackerapp.crypto.presentation.coin_list.components.previewCoin
 import com.mramallo.cryptotrackerapp.ui.theme.CryptoTrackerAppTheme
@@ -36,6 +51,11 @@ fun CoinListScreen(
 ) {
     val isRefreshing by remember { mutableStateOf(state.isLoading) }
     val coroutineScope = rememberCoroutineScope()
+    val contentColor = if (isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Color.Black
+    }
 
     if (state.isLoading) {
         Box(
@@ -55,14 +75,44 @@ fun CoinListScreen(
             }
         ) {
             if (state.coins.isEmpty()) {
-                Box(
+                Column(
                     modifier = modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                        .fillMaxSize()
+                        .padding(horizontal = 40.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Text(text = "No coins found")
+                    Image(
+                        imageVector = ImageVector.vectorResource(
+                            id = R.drawable.empty_coin
+                        ),
+                        contentDescription = "empty coin",
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                        modifier = Modifier.size(160.dp)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Oops!",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = contentColor,
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Something went wrong!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = contentColor,
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        "We did not find any cryptocurrencies at the moment. Please check your connection and try again."
+                    )
                 }
-                // TODO - ADD EMPTY STATE DESIGN
             }
 
             LazyColumn(
